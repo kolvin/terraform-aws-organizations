51278a528308adc389673d0ad13ea23f54e4f85c
kolvin <15124052+Kolvin@users.noreply.github.com>
2022-08-25 23:29:07 +0100

    fix(org): reference correct collection of locals
    Signed-off-by: kolvin <15124052+Kolvin@users.noreply.github.com>

--- locals.tf
+++ locals.tf
@@ -1,4 +1,12 @@
 locals {
-  accounts            = { for account in var.accounts : account.account_name => account }
-  organizations_units = toset([for account in var.accounts : account.org_unit if account.org_unit != ""])
+  accounts           = { for account in var.accounts : account.account_name => account }
+  organization_units = toset([for account in var.accounts : account.org_unit if account.org_unit != ""])
+
+  organization_accounts = { for account in var.org_accounts : account.account_name => {
+    account_name               = account.account_name
+    email                      = account.email
+    iam_user_access_to_billing = account.iam_user_access_to_billing
+    org_unit                   = account.org_unit == "" ? aws_organizations_organization.this.roots[0].id : aws_organizations_organizational_unit.this[account.org_unit].id
+    }
+  }
 }
